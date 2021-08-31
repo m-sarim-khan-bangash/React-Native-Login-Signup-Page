d6cf6592eadcd7ab46cc28b5da6f092b57222ad0
Muhammad Sarim Khan Bangash <75114586+m-sarim-khan-bangash@users.noreply.github.com>
2021-09-01 00:15:23 +0500

    Minor changes in signup page

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -49,19 +49,10 @@ const Signup = props => {
             <Text style={styles.heading}>Signup Page</Text>
           </View>
           <View>
-            <Icon name="bell" size={30} color="#2980b9" />
+            <Icon name="bell" size={25} color="#2980b9" />
           </View>
         </View>
-        <Text
-          style={{
-            fontSize: 32,
-            fontWeight: '700',
-            color: '#fff',
-            marginBottom: 10,
-            marginTop: 50,
-          }}>
-          SignUp Here
-        </Text>
+        <Text style={styles.signupHeading}>SignUp Here</Text>
         <View>
           <Image
             style={styles.logo}
@@ -70,23 +61,36 @@ const Signup = props => {
             }}
           />
         </View>
-        <TextInput
-          onChangeText={text => console.log(text)}
-          style={styles.input}
-          placeholder="Enter Username"
-        />
-        <TextInput
-          onChangeText={text => setEmail(text)}
-          keyboardType={'email-address'}
-          style={styles.input}
-          placeholder="Enter Email"
-        />
-        <TextInput
-          onChangeText={text => setPassword(text)}
-          style={styles.input}
-          placeholder="Enter Password"
-          secureTextEntry={true}
-        />
+        <View style={styles.viewText}>
+          <Icon name="user" size={20} color="#f4f4f4" />
+          <TextInput
+            onChangeText={text => console.log(text)}
+            style={styles.input}
+            placeholder="Enter Username"
+            placeholderTextColor="#ccc"
+          />
+        </View>
+
+        <View style={styles.viewText}>
+          <Icon name="envelope-o" size={20} color="#f4f4f4" />
+          <TextInput
+            onChangeText={text => console.log(text)}
+            style={styles.input}
+            placeholder="Enter Email"
+            placeholderTextColor="#ccc"
+          />
+        </View>
+
+        <View style={styles.viewText}>
+          <Icon name="lock" size={20} color="#f4f4f4" />
+          <TextInput
+            onChangeText={text => console.log(text)}
+            style={styles.input}
+            placeholder="Enter Password"
+            secureTextEntry={true}
+            placeholderTextColor="#ccc"
+          />
+        </View>
 
         <Text style={styles.forgetPassword}>Forgot Password?</Text>
         <TouchableOpacity style={styles.btn} onPress={register}>
@@ -97,7 +101,7 @@ const Signup = props => {
         </Text>
         <TouchableOpacity
           onPress={() => props.navigation.navigate('Login Page')}>
-          <Text style={{color: '#ddd', marginTop: 10}}>Login Here</Text>
+          <Text style={{color: '#7ed6df', marginTop: 10}}>Login Here</Text>
         </TouchableOpacity>
       </View>
     </TouchableWithoutFeedback>
@@ -108,8 +112,14 @@ const styles = StyleSheet.create({
   view: {
     backgroundColor: '#192a56',
     flex: 1,
-    // paddingTop: 100,
     alignItems: 'center',
+  },
+  signupHeading: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: '#fff',
+    marginBottom: 10,
+    marginTop: 50,
   },
   btn: {
     width: '62%',
@@ -121,21 +131,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   input: {
-    borderBottomWidth: 1,
-    borderBottomColor: '#fff',
-    width: '60%',
     color: '#fff',
     fontSize: 16,
-    marginTop: 10,
+    paddingHorizontal: 10,
   },
   forgetPassword: {
-    color: '#ccc',
+    color: '#7ed6df',
     marginTop: 10,
     marginLeft: 110,
   },
   logo: {
     height: 120,
     width: 120,
+    marginTop: 15,
+    marginBottom: 15,
   },
   header: {
     width: '100%',
@@ -151,6 +160,16 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     color: '#222',
   },
+  viewText: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#fff',
+    width: '60%',
+    marginTop: 12,
+  },
 });
 
 export default Signup;
+
