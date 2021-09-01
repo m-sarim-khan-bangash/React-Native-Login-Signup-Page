753808c8e09c75e13952e6b8703f915c5d6f114f
Muhammad Sarim Khan Bangash <75114586+m-sarim-khan-bangash@users.noreply.github.com>
2021-09-01 22:11:04 +0500

    Minor updates in Login Page

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -6,86 +6,115 @@ import {
   TouchableOpacity,
   TextInput,
   Image,
+  ImageBackground,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+const image = {
+  uri: 'https://iammagnus.com/wp-content/uploads/2016/05/website-design-background-1.jpg',
+};
+
 const Login = props => {
   return (
-    <View style={styles.view}>
-      <Text style={styles.loginHeading}>Login Here</Text>
-      <View>
-        <Image
-          style={styles.logo}
-          source={{
-            uri: 'http://www.wrnrtv.com/wp-content/uploads/2017/05/male.png',
-          }}
-        />
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <View style={{flex: 1, backgroundColor: 'rgba(0,0,0,0.4)'}}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            Keyboard.dismiss();
+          }}>
+          <View style={styles.view}>
+            <Text style={styles.loginHeading}>Login Here</Text>
+            <View>
+              <Image
+                style={styles.logo}
+                source={{
+                  uri: 'http://www.wrnrtv.com/wp-content/uploads/2017/05/male.png',
+                }}
+              />
+            </View>
+            <View style={styles.viewText}>
+              <Icon name="user" size={20} color="#f4f4f4" />
+              <TextInput
+                onChangeText={text => console.log(text)}
+                style={styles.input}
+                placeholder="Enter Username"
+                placeholderTextColor="#ccc"
+              />
+            </View>
+            <View style={styles.viewText}>
+              <Icon name="lock" size={20} color="#f4f4f4" />
+              <TextInput
+                onChangeText={text => console.log(text)}
+                style={styles.input}
+                placeholder="Enter Password"
+                secureTextEntry={true}
+                placeholderTextColor="#ccc"
+              />
+            </View>
+            <Text style={styles.forgetPassword}>Forgot Password?</Text>
+            <TouchableOpacity
+              style={styles.btn}
+              onPress={() => alert('User Login Successfully!')}>
+              <Text style={{color: '#000'}}>Login</Text>
+            </TouchableOpacity>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: 14,
+              }}>
+              <Text
+                style={{color: '#fff', fontSize: 12, paddingHorizontal: 10}}>
+                Dont have an account?{' '}
+              </Text>
+              <TouchableOpacity
+                onPress={() => props.navigation.navigate('Signup')}>
+                <Text
+                  style={{
+                    color: '#7ed6df',
+                    fontSize: 14,
+                    paddingHorizontal: 10,
+                  }}>
+                  Signup Now
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <Text style={{color: '#fff', paddingVertical: 12, fontSize: 20}}>
+              ___________OR___________
+            </Text>
+            <TouchableOpacity style={styles.btnGoogle}>
+              <Text style={{color: '#fff', fontSize: 17}}>
+                Signin With Google
+              </Text>
+              <Icon name="google" size={25} color="#f4f4f4" />
+            </TouchableOpacity>
+          </View>
+        </TouchableWithoutFeedback>
       </View>
-      <View style={styles.viewText}>
-        <Icon name="user" size={20} color="#f4f4f4" />
-        <TextInput
-          onChangeText={text => console.log(text)}
-          style={styles.input}
-          placeholder="Enter Username"
-          placeholderTextColor="#ccc"
-        />
-      </View>
-      <View style={styles.viewText}>
-        <Icon name="lock" size={20} color="#f4f4f4" />
-        <TextInput
-          onChangeText={text => console.log(text)}
-          style={styles.input}
-          placeholder="Enter Password"
-          secureTextEntry={true}
-          placeholderTextColor="#ccc"
-        />
-      </View>
-      <Text style={styles.forgetPassword}>Forgot Password?</Text>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => alert('User Login Successfully!')}>
-        <Text style={{color: '#000'}}>Login</Text>
-      </TouchableOpacity>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          marginTop: 14,
-        }}>
-        <Text style={{color: '#fff', fontSize: 12, paddingHorizontal: 10}}>
-          Dont have an account?{' '}
-        </Text>
-        <TouchableOpacity onPress={() => props.navigation.navigate('Signup')}>
-          <Text style={{color: '#7ed6df', fontSize: 14, paddingHorizontal: 10}}>
-            Signup Here
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <Text style={{color: '#fff', paddingVertical: 12, fontSize: 20}}>
-        ___________OR___________
-      </Text>
-      <TouchableOpacity
-        style={styles.btnGoogle}
-        onPress={() => alert('User Login Successfully!')}>
-        <Text style={{color: '#fff', fontSize: 17}}>Signin With Google</Text>
-        <Icon name="google" size={25} color="#f4f4f4" />
-      </TouchableOpacity>
-    </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
   view: {
-    backgroundColor: '#192a56',
+    // backgroundColor: '#192a56',
+    // backgroundColor: '#2c3e50',
     flex: 1,
     alignItems: 'center',
   },
   loginHeading: {
-    fontSize: 32,
+    fontSize: 38,
+    letterSpacing: 1,
     fontWeight: '700',
     color: '#fff',
     marginBottom: 10,
-    marginTop: 50,
+    marginTop: 80,
   },
   btn: {
     width: '62%',
@@ -128,9 +157,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderBottomWidth: 1,
     borderBottomColor: '#fff',
-    width: '60%',
+    width: '62%',
     marginTop: 12,
   },
 });
 
 export default Login;
+
